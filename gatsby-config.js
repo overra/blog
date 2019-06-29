7ff6d188d78abe9fdf88fd367d3f848075221667
Adam Snodgrass <overra@gmail.com>
2019-06-29 10:36:08 -0500

    add GA tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-32307743-8`,
       },
     },
     `gatsby-plugin-feed`,
